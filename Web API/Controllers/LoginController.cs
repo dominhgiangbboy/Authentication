7db51d6d858cba955bb40ea0ef77b4fd2c469609
dominhgiangbboy <dominhgianga5nc@gmail.com>
2019-11-26 10:36:22 +0700

    init

---
+++ Web API/Controllers/LoginController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Web_API.Models;
+
+namespace Web_API.Controllers
+
+{
+    public class LoginController : ApiController
+    {
+        // GET: api/Logins
+        [HttpGet]
+        public string Get(User UserModel)
+        {
+            string Status = null;
+            string header = "" /*HttpRequestHeader("Authorization")*/ ;
+            using (LoginEntities db = new LoginEntities())
+                if (header == "Incorrect Username and password") {
+                Status = "Chua dang nhap thanh cong";
+            }
+            else
+            {
+                    Status = UserModel.Account_name; 
+            }
+            return Status;
+        }
+
+
+        // GET: api/Login/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Login
+        [HttpPost]
+        public object Post([FromBody]User UserModel)
+        {
+
+            string token = "";
+            User Userinfo = new User();
+            using (LoginEntities db = new LoginEntities())
+            {
+                Userinfo = db.Users.Where(x => x.Account_name == UserModel.Account_name
+                                             && x.Password == UserModel.Password).SingleOrDefault<User>();
+            };
+            if (Userinfo == null)
+            {
+
+                UserModel.LoginErrorMessage = "Incorrect Username and password ";
+                // return View("Index", UserModel);
+                token = "Incorrect Username and password";
+                //System.Web.HttpContext.Current.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+
+            }
+            else
+            {
+                //Session["UserAccount"] = UserModel.Account_name;
+                token = "successfully logged in";
+                System.Web.HttpContext.Current.Response.AppendHeader("Status", "Successfully");
+                //System.Web.HttpContext.Current.Response.Redirect("~/Home/Index");
+            }
+            return token;
+        }
+    }
+}
---
+++ Web API/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+
+namespace Web_API
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+        }
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            var context = HttpContext.Current;
+            var response = context.Response;
+
+            // enable CORS  
+       
+
+            if (context.Request.HttpMethod == "OPTIONS")
+            {
+                response.AddHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+                response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                
+                response.End();
+            }
+        }
+    }
+
+}
